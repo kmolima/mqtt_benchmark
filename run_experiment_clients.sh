b996196fab289d5ed0f0c2b2d46ac8b9d91a4443
Keila <keilalima.cv@gmail.com>
2024-10-30 03:05:04 +0100

    Adjusted parameters in clients automates execution.

--- run_experiment_clients.sh
+++ run_experiment_clients.sh
@@ -8,16 +8,32 @@ fi
 
 if [ -z "$2" ]
   then
+    echo "Please supply the log sub path"
+    exit 1
+fi
+
+if [ -z "$3" ]
+  then
     echo "Please supply docker stats file name"
+    exit 1
+fi
+
+if [ -z "$4" ]
+  then
+    echo "Please supply the CA cert path"
     exit 1
 fi
 
 
 export QOS=$1
 
-echo "Running clients for QOS: $1 and saving docker stats in $2"
+export LOG_PATH=$3
 
-sh record_stats.sh $2 > /dev/null 2>&1 &
+export CERT=$4
+
+echo "Running clients for QOS: $1, log sub path $2 and saving docker stats in $3"
+
+sh record_stats.sh $3 > /dev/null 2>&1 &
 
 docker compose -f docker-compose-clients up &
 
