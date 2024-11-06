a7e5d751c9a22f46cf9b4199fdadcd8cf352c9c9
Keila <keilalima.cv@gmail.com>
2024-11-06 16:48:37 +0100

    Adjusted load execution

--- run_load.py
+++ run_load.py
@@ -7,13 +7,15 @@ def run_docker_compose(compose_file):
     try:
         with subprocess.Popen(
             ["docker-compose", "-f", compose_file, "up", "-d",
-             "--exit-code-from finish", "--abort-on-container-exit"]
+             "--exit-code-from", "finish", "--abort-on-container-exit"]
         ) as proc:
             proc.wait()
             print("Docker Compose started successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e.returncode}")
         print(e.output)
+        stop_docker_compose(compose_file)
+        proc.kill()
 
 
 def stop_docker_compose(compose_file):
@@ -51,23 +53,27 @@ def main():
         with subprocess.Popen(
             ["/bin/bash", "record_stats.sh", args.stats]
         ) as proc:
-            proc.wait()
             print("Docker stats recording started successfully.")
+
+            for _exec in range(numexecs):
+                # Run MQTT Clients
+                print(f'Running execution {_exec}. . .')
+                run_docker_compose(load_docker_compose)
+                time.sleep(60)  # time between load repetition run -- KEEP ALIVE
+
+    except Exception as e:
+        stop_docker_compose(load_docker_compose)
+        print(f'Error executing experiment docker compose: {e}')
+
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e.returncode}")
         print(e.output)
 
-    for exec in range(numexecs):
-        try:
-            # Run MQTT Clients
-            run_docker_compose(load_docker_compose)
-            time.sleep(30)  # time between load repetition run
+    finally:
+        proc.kill()
+        print(f'Finished execution {exec}')
 
-        except Exception as e:
-            stop_docker_compose(load_docker_compose)
-            print(f'Error executing experiment containers: {e}')
-        finally:
-            print(f'Finished execution {exec}')
+
 
 
 if __name__ == "__main__":
